514ff7e0b2686675f6b42f49ceba7bc9c7099d9b
KiMJH <cale_kim@naver.com>
2023-07-28 16:04:05 +0900

    Root & CSS Import

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,16 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+  //   return문 마다 Root가 1개씩(Only) 있어야 한다. (중요!!)
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+      </div>
+      <div className="expense-item__price">$294.67</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;

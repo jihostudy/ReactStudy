a3fce5fb5a709f0cedb2a9065e108e06d32509c4
KiMJH <cale_kim@naver.com>
2023-07-28 15:52:15 +0900

    컴포넌트 생성하기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      {/* 리액트에서는 Component들은 꼭 대문자로 시작해야한다 그래야 User Component임을 안다 */}
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }

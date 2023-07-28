db26b0f7358574ed3e0075583293b06aa65903d1
KiMJH <cale_kim@naver.com>
2023-07-28 16:51:58 +0900

    props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,13 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
-  // 컴포넌트는 재사용가능하게끔 해야함
-  // 따라서, Hard Coding된 것들을 일반적 JS 코드로 작성가능 + JSX의 {}안에 작성하면 된다.
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
       </div>
-      <div className="expense-item__price">${expenseAmount}</div>
+      <div className="expense-item__price">${props.amount}</div>
     </div>
   );
 }

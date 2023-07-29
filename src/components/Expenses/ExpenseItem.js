e18363061bdad413a9638e65c8ed3ecd16bb5650
KiMJH <cale_kim@naver.com>
2023-07-29 21:37:09 +0900

    리액트가 JSX코드를 해석하는 방법(State필요성)

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -9,15 +9,18 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  let title = props.title;
+
   const clickHandler = () => {
-    console.log("Clicked!!!");
+    title = "Changed!";
+    console.log(title);
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
       <button onClick={clickHandler}>Change Title</button>

b33e4ce2873c77ca662e196bdf8ed273680700f7
KiMJH <cale_kim@naver.com>
2023-07-29 16:05:05 +0900

    Expenses 상위 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Expenses from "./components/Expenses";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
@@ -23,30 +24,7 @@ function App() {
     },
   ];
 
-  return (
-    <div>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
-    </div>
-  );
+  return <Expenses expenseData={expenses}></Expenses>;
 }
 
 export default App;
---
+++ src/components/Expenses.js
@@ -0,0 +1,34 @@
+//Style
+import "./Expenses.css";
+
+//Module
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses(props) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={props.expenseData[0].title}
+        amount={props.expenseData[0].amount}
+        date={props.expenseData[0].date}
+      />
+      <ExpenseItem
+        title={props.expenseData[1].title}
+        amount={props.expenseData[1].amount}
+        date={props.expenseData[1].date}
+      />
+      <ExpenseItem
+        title={props.expenseData[2].title}
+        amount={props.expenseData[2].amount}
+        date={props.expenseData[2].date}
+      />
+      <ExpenseItem
+        title={props.expenseData[3].title}
+        amount={props.expenseData[3].amount}
+        date={props.expenseData[3].date}
+      />
+    </div>
+  );
+}
+
+export default Expenses;

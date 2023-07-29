d23d386b8dad7b2be6315d5c1c8c8519001b70d3
KiMJH <cale_kim@naver.com>
2023-07-29 23:09:00 +0900

    useState에 관한 DeepDiving

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -9,9 +9,13 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  //props.title은 상태의 기본값 => useState는 길이가 2인 배열 반환
+  //props.title은 상태의 기본값 : 처음 불릴때만 사용되는 값이다 => useState는 길이가 2인 배열 반환
   //첫번째 원소 : 현재 상태, 두번째 원소 : Setter 함수
+  //useState가 관리하는 변수는 우리에게 보이진 않지만, 어떤 변수(메모리)의 값을 바꾸는 것임. 그것의 상태가 첫번째 원소임.
+  //useState는 Per Component Instance로 만들어진다!! (그래서 해당 title만 바뀜)
+  //컴포넌트들은 같게 생겼지만 모두 독립적으로 존재한다.
   const [title, setTitle] = useState(props.title);
+  console.log("ExpenseItem evaluated by React");
 
   const clickHandler = () => {
     setTitle("Updated!");
